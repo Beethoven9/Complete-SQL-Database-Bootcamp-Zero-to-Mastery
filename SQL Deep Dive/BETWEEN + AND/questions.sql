2136b1c01540d81467f31b2ef4030df8b28f3e47
Beethoven9 <alfredo.chalabe@gmail.com>
2022-12-08 10:49:50 -0300

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -2,10 +2,20 @@
 -- (include 30 and 50 in the results)
 
 /*
-* Write your query here
+
+SELECT firstname, lastname, income, age from customers
+WHERE (age BETWEEN 30 AND 50) AND (income < 50000)
+
 */
+2362 records
+
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
-* Write your query here
+
+SELECT AVG(income) from customers
+WHERE (age BETWEEN 20 AND 50)
+
 */
+
+59361.925908612832
